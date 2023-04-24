c85939700afedf36f9f3eca4935be5cea562ffd3
rjalabado <rjalabado@gmail.com>
2023-04-24 09:01:47 -0700

    Initial Commit

---
+++ Folder Opener 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Folder Opener 3
+//
+//  Created by RRAlabado on 4/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
